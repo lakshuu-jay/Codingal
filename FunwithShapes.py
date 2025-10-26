518ae9e31350faacc8a08f33c3728fe2f90660c1
(lakshuu-jay) <lakshu.jk13@gmail.com>
2025-10-26 13:10:14 +0400

    turtle

---
+++ FunwithShapes.py
@@ -0,0 +1,43 @@
+import turtle
+
+screen = turtle.Screen()
+screen.bgcolor("lightblue") 
+screen.title("Polygons with Turtle")
+
+
+t = turtle.Turtle()
+t.speed(3)  
+
+t.penup()
+t.goto(-200, 100)  
+t.pendown()
+t.color("red", "yellow")  
+t.begin_fill()
+for _ in range(3):
+    t.forward(100)
+    t.left(120)
+t.end_fill()
+
+t.penup()
+t.goto(50, 100)
+t.pendown()
+t.color("blue", "green")
+t.begin_fill()
+for _ in range(2):
+    t.forward(150) 
+    t.left(90)
+    t.forward(80)
+    t.left(90)
+t.end_fill()
+
+t.penup()
+t.goto(-50, -100)
+t.pendown()
+t.color("purple", "pink")
+t.begin_fill()
+for _ in range(6):
+    t.forward(70)
+    t.left(60)
+t.end_fill()
+
+turtle.done()
