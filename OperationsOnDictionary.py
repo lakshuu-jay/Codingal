7242bdefade3f706ffbcdbb554038c68fa55fe89
(lakshuu-jay) <lakshu.jk13@gmail.com>
2025-10-31 15:56:57 +0400

    DataStructures2

--- OperationsOnDictionary.py
+++ OperationsOnDictionary.py
@@ -6,13 +6,3 @@ my_dict = {'name':'Jack','age':26}
 print(my_dict['name'])
 print(my_dict.get("age"))
 
-my_dict['age'] = 27
-print(my_dict)
-
-my_dict['address']='Downtown'
-print(my_dict)
-
-my_dict.pop('age')
-print(my_dict)
-
-print("Address: ",my_dict.get('address'))
