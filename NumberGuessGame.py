8830356bd821a808777f0263299a2da7c3976a16
(lakshuu-jay) <lakshu.jk13@gmail.com>
2025-11-02 09:41:14 +0400

    List

---
+++ NumberGuessGame.py
@@ -0,0 +1,15 @@
+import random
+
+numbers = [5, 10, 15, 20, 25, 30]
+original_number = random.choice(numbers)
+
+guess = int(input("Guess the number: "))
+
+if guess == original_number:
+    print("Correct! You guessed the right number!")
+elif guess > original_number:
+    print("Your guess is higher than the actual number. Try again!")
+else:
+    print("Your guess is lower than the actual number. Try again!")
+
+print(f"(The correct number was {original_number})")
