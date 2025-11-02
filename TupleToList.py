d213b78be62c1d835232091c79402f3578919abf
(lakshuu-jay) <lakshu.jk13@gmail.com>
2025-11-02 12:14:43 +0400

    DataStructures2

---
+++ TupleToList.py
@@ -0,0 +1,6 @@
+personal_details = ("Lakshanya", "Jayakumar", 13, 150, 45, "Science")
+
+details_list = list(personal_details)
+
+print("Tuple:", personal_details)
+print("Converted List:", details_list)
