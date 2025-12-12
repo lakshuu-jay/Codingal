b5f34d7739f2ada239eff12870f9f5e164302a4c
(lakshuu-jay) <lakshu.jk13@gmail.com>
2025-12-12 22:02:53 +0400

    OperationsOnFile2

--- FileMerge.py
+++ FileMerge.py
@@ -4,8 +4,8 @@ with open("Codingal.txt")as fp:
 with open("sample_doc.txt")as fp:
     data2=fp.read()
 
-data+="\n"
-data+=data2
+data1 += "\n"
+data1 +=data2
 print("Merging two files....")
 with open("MergedFile.txt","w")as fp:
     fp.write(data1)
