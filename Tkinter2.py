511713a743453c761c1f43b6722355ee1a4f0eb1
(lakshuu-jay) <lakshu.jk13@gmail.com>
2025-12-13 12:52:39 +0400

    Tkinter1

--- Tkinter2.py
+++ Tkinter2.py
@@ -6,7 +6,7 @@ for i in range(3):
     for j in range(3):
         frame=tk.Frame(
             master=window,
-            relief=tk.RAISED
+            relief=tk.RAISED,
             borderwidth=1
         )
         frame.grid(row=i,column=j,padx=5,pady=5)
