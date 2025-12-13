e648a757f40bc1c498ebf8889ef095e4c05b3baf
(lakshuu-jay) <lakshu.jk13@gmail.com>
2025-12-13 10:10:21 +0400

    OperationsOnFile2

--- DuplicateLines.py
+++ DuplicateLines.py
@@ -8,7 +8,7 @@ print("Eliminating duplicate lines....")
 for line in inputFile:
     clean = line.strip()
     if line not in lines_seen_so_far:
-        outputFile.write(line)
+        outputFile.write(clean)
         lines_seen_so_far.add(clean)
 
 inputFile.close()
