3db621bfbee5ada30fafaa7444296a2da4254051
(lakshuu-jay) <lakshu.jk13@gmail.com>
2025-12-15 10:47:59 +0400

    Tkinter2

---
+++ ImageMakesItBetter!.py
@@ -0,0 +1,17 @@
+from tkinter import *
+from PIL import Image, ImageTk
+
+root= Tk()
+root.title("Image Example")
+root.geometry("480x480")
+
+upload=Image.open("spiderman.jpg")
+image=ImageTk.PhotoImage(upload)
+
+label=Label(root,image=image,height=350,widht=300)
+label.place(x=50,y=0)
+
+label2=Label(root,text="This is how you add an image in Tkinter window")
+label2.place(x=40,y=360)
+
+root.mainloop()
