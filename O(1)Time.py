6f75d3bd6337f737616e0829e33e23ad5caf5e2b
(lakshuu-jay) <lakshu.jk13@gmail.com>
2025-12-20 10:48:36 +0400

    AsymptoticNotation

---
+++ O(1)Time.py
@@ -0,0 +1,9 @@
+def printnumber(n):
+    iteration = 0
+    print("The number entered by user is",n)
+    iteration += 1
+    print("Total iterations done by the code is",iteration)
+
+printnumber(10)
+printnumber(20)
+print("\nWith any 'n' the time taken by our code won't change")
