88ef29da453b8e80793322f51b5bb7c92f69a408
(lakshuu-jay) <lakshu.jk13@gmail.com>
2025-12-22 22:01:21 +0400

    AnalysisandSpaceComplexity

--- GiveMeSomeSpace.py
+++ GiveMeSomeSpace.py
@@ -1,18 +1,13 @@
 def sum(n):
-    return n*(n+1)/2
-sum(20)
+    return n * (n + 1) / 2
 
 def arraysum(a):
-    sum=0
+    sum = 0
     for i in a:
-        sum=sum+i
-    return(sum)
-
-a=[12,3,4,15]
-arraysum(a)
+        sum = sum + i
+    return sum
 
 def summ(n):
-    if(n<=0):
+    if (n <= 0):
         return
-    return n+summ(n-1)
-summ(10)
+    return n + summ(n - 1)
