02b40f789bb1f7bc622ac4d36ec11e106e86a0c3
(lakshuu-jay) <lakshu.jk13@gmail.com>
2026-01-02 15:51:05 +0400

    IntroToBitManipulation

---
+++ ImplementBitwise.py
@@ -0,0 +1,32 @@
+num1 = 10
+num2 = 4
+
+#AND
+print("num 1 & num 2 =",num1 & num2)
+
+#OR
+print("\nnum1 | num2 = ", num1 | num2)
+
+#NOT
+print("\n~num1 =",num1)
+
+#XOR
+print("\nnum1 ^ num2=",num1 ^ num2)
+
+num1=10
+num2= 4
+
+#Right Shift of num1
+print("\nnum1 >> 1=",num1 >> 1)
+
+#Right Shift of num2
+print("\nnum2 >> 1 =",num2 >> 1)
+
+num1= 10
+num2=4
+
+#Left Shift of num1
+print("\nnum1 << 1 =", num1<< 1)
+
+#Left Shift of num2
+print("\nnum2 << 1=", num2 << 1)
