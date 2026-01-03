69decac453269a53f0e456220a0baf24216618df
(lakshuu-jay) <lakshu.jk13@gmail.com>
2026-01-03 17:11:52 +0400

    BitPlay1

--- FirstSetBit.py
+++ FirstSetBit.py
@@ -1,12 +1,11 @@
 def firstSetBit(n):
     count = 1
-    while (n):
-        if n &1 ==1:
-           break
-    count +=1
-    n = n >>1
-    print("Position of the first set bit:",count)
+    while(n):
+        if (n & 1 == 1):
+            print("Position of the first set bit:", count)
+            return
+        n >>= 1
+        count += 1
 
-number = int(input("Enter a number:"))
-
+number = int(input("Enter a number: "))
 firstSetBit(number)
